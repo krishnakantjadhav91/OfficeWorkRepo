27b61263747ea14cb59b873f861ce72b06f920e7
Krishnakant.Jadhav <Krishnakant.Jadhav@MXITD031.MAXIMAPUNE.COM>
2018-03-08 18:49:49 +0530

    Code changes made.

--- src/test/java/tests/Test2.java
+++ src/test/java/tests/Test2.java
@@ -1,15 +1,34 @@
 package tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class Test2 
 {
+	WebDriver dr ;
 	@Test
 	public void T1()
 	{
-		System.out.println("Run is executed...");
+		System.out.println("Testcase1 is running...");
+		System.setProperty("webdriver.chrome.driver", "D:\\Softwares\\chromedriver_win32\\chromedriver.exe") ;
+		dr = new ChromeDriver() ;
+		System.out.println("T1 is executed...");
 	}
 	
+	@Test
+	public void T2()
+	{
+		System.out.println("Testcase2 is running...");
+		System.setProperty("webdriver.chrome.driver", "D:\\Softwares\\chromedriver_win32\\chromedriver.exe") ;
+		dr = new ChromeDriver() ;
+		System.out.println("T2 is executed...");		
+	}
 	
-
+	@AfterMethod
+	public void tearOut()
+	{
+		dr.quit();
+	}
 }
